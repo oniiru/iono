8fb7e9b27520993af9cdbd8cdfcaef14610affc4
oniiru <oneal.andrew@gmail.com>
2013-03-27 01:14:36 -0700

    asdf

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'scoutthr_SolidWize');
+define('DB_NAME', 'rawr');
 
 /** MySQL database username */
-define('DB_USER', 'scoutthr_solidw');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'AjarFamilyRosesSkiff77');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
